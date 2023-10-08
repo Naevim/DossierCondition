277a25cf49ab19e0235a8a92697783e3cbef9347
Naevim <146922198+Naevim@users.noreply.github.com>
2023-10-08 11:35:08 +0200

    Add files via upload

---
+++ exercice_condition.js
@@ -0,0 +1,16 @@
+// Condition
+"use strict";
+
+let a=4
+let b=4
+let c=3
+
+if(a==b){
+    console.log("egal a b");
+}else if(a==c){
+    console.log("egal a c");
+}
+else {
+    console.log("egal a rien");
+}
+
